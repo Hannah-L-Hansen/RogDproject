800f4dc4102339b408f3caabdeb9a396f02f4a0b
Hannah-L-Hansen <43685263+Hannah-L-Hansen@users.noreply.github.com>
2018-10-04 10:33:26 +0200

    new dataframe bmpPlot30
    Creating dataframe that contains all desired data for use when plotting

--- scripts/grouping.R
+++ scripts/grouping.R
@@ -1,43 +1,36 @@
 #grouping biogas calc for plotting
 
 
-# BMP1p <- bmp1p %>%
-#   group_by(lab) %>%
-#   summarise(
-#   )
-# 
-# BMP0.5p <- bmp0.5p %>%
-#   group_by(lab) %>%
-#   summarise(
-#     BMP0.5p= mean(BMP, na.rm = TRUE)                 
-#   )
-
-#Creating data frame with testno, time, lab no, substrate, bmp1p and Bmp0.5p
-
-# bmpCompare <- bmp0.5p[,c('test','lab','substrate','time.d','BMP')]
-# 
-# #extracting BMP column from bmp1p:
-# 
-# bmp1percent <- bmp1p[,c('BMP')]
-# 
-# #addition of bmp1percent to bmpCompare
-# 
-# bmpCompare <- as.data.frame(cbind(bmpCompare,bmp1percent))
-# 
-# #Naming columns in bmpCompare
-# 
-# names(bmpCompare) <- c('test','lab','substrate','time','bmp0.5p','bmp1p')
-
-#merging dataframes bmp0.5p and bmp1%
+#merging dataframes bmp0.5p and bmp1% to create new dataframe bmpPlot to use for plotting
 
 bmpMerge <- merge(x = bmp0.5p, y = bmp1p, by = c('test','lab','substrate'), all = TRUE, suffixes = c('.bmp0.5p','.bmp1p'))
 
-
 bmpPlot <- bmpMerge[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p')]
 
-#navngiv kolonner
-#names(bmpPlot) <- c('test')
-
 #add bmp20 and bmp30
-#add gross vs net
+
+
+#add bmp 20
+bmpMerge20 <- merge(x = bmpPlot, y = bmp20, by = c('test','lab','substrate'), all = TRUE, suffixes = c('.bmpPlot','.bmp20'))
+
+bmpPlot20 <- bmpMerge20[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d','BMP')]
+
+#naming bmpPlot20
+
+names(bmpPlot20) <- c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d.BMP20','BMP20')
+
+#Add bmp30
+
+bmpMerge30 <- merge(x = bmpPlot20, y = bmp30, by = c('test','lab','substrate'), all = TRUE)
+
+bmpPlot30 <- 
+  bmpMerge30[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d.BMP20','BMP20','time.d','BMP')]
+
+#naming bmpPlot30
+
+names(bmpPlot30) <- 
+  c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d.BMP20','BMP20','time.d.BMP30','BMP30')
+
+
+#add gross vs net --> figure out if bmp0.5pg is possible to add first!
 
