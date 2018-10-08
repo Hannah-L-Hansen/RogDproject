eeab9de712d2d13f5ab3d87527f59f8776597577
Hannah-L-Hansen <43685263+Hannah-L-Hansen@users.noreply.github.com>
2018-10-08 14:18:39 +0200

    multiple changes (FY)
    in bmpPlots script Plots made with dyplr with help from Sasha
    
    In "grouping" script Adding rate.met columns to BmpPlot30
    
    Creating new script

--- scripts/bmpPlots.R
+++ scripts/bmpPlots.R
@@ -4,24 +4,25 @@
 
 #creating and adding rank columns
 
-labRankDataFrame <- bmpPlot30 %>% 
+labRankDataFrame <- bmpPlot30 %>%
   group_by(lab) %>%
   summarise() %>%
   arrange(lab)%>%
   mutate(labPlotRank=row_number())
 
-substrateRankDataFrame <- bmpPlot30 %>% 
+substrateRankDataFrame <- bmpPlot30 %>%
   group_by(substrate) %>%
   summarise() %>%
   arrange(substrate)%>%
   mutate(substratePlotRank=row_number())
+
 
 #merging labRank and substrateRank onto bmpPlot30
 
 bmpPlot30 <- merge(x = bmpPlot30, y = labRankDataFrame, by = c('lab'), all.x = TRUE)
 
 bmpPlot30 <- merge(x = bmpPlot30, y = substrateRankDataFrame, by = c('substrate'), all.x = TRUE)
-  
+
 
 reshapeBMP0.5pBMP1p <- function(x){
 return(
@@ -36,17 +37,17 @@ return(
             substrate=substrate, 
             time.d=time.d.bmp0.5p, 
             BMP=BMP.bmp0.5p) %>%
-  mutate(yIndex=0),
+  mutate(calcMethod='0.5p'),
     x %>% 
     select( 
       substratePlotRank=substratePlotRank,
       labPlotRank=labPlotRank,
-            test=test, 
+            test=test,
             lab=lab, 
             substrate=substrate, 
             time.d=time.d.bmp1p, 
             BMP=BMP.bmp1p) %>%
-            mutate(yIndex=1)
+            mutate(calcMethod ='1p')
             ))
 }
 
@@ -55,25 +56,38 @@ dfPlot0.5And1 <- reshapeBMP0.5pBMP1p(bmpPlot30)
 
 #Bar plot 
 
-ggplot(
-  data = dfPlot0.5And1, 
-  aes(
-    x=1+((labPlotRank-1)*2+yIndex),
-    y=BMP,
-    fill=lab
-  )) +
-  geom_col()+
-  facet_wrap(facets = 'substrate')+
-  labs(x='lab')
+# ggplot(
+#   data = dfPlot0.5And1, 
+#   aes(
+#     x=1+((labPlotRank-1)*2+yIndex),
+#     y=BMP,
+#     fill=lab
+#   )) +
+#   geom_col()+
+#   facet_wrap(facets = 'substrate')+
+#   labs(x='lab')
 
 #boxplot by substrate
 
-ggplot(
-  data = dfPlot0.5And1, 
-  aes(
-    x=substrate,
-    y=BMP,
-    group=substrate)) +
-  facet_grid(yIndex~.)+
-  geom_boxplot(width = 0.3)
-  
+# ggplot(
+#   data = dfPlot0.5And1, 
+#   aes(
+#     x=substrate,
+#     y=BMP,
+#     group=substrate)) +
+#   facet_grid(yIndex~.)+
+#   geom_boxplot(width = 0.3)
+ 
+#boxplot
+ggplot(data = dfPlot0.5And1, aes(substrate, BMP, fill = factor(calcMethod))) +
+  geom_boxplot()+
+  #facet_grid(.~ substrate) +
+  labs(x='substrate',fill = 'method')
+
+ 
+
+#bar chart
+ggplot(data = dfPlot0.5And1, aes(lab, BMP, fill = factor(calcMethod))) +
+       geom_col(position = 'dodge')+
+       facet_wrap(~ substrate) +
+       labs(x='lab',fill = 'method')
--- scripts/grouping.R
+++ scripts/grouping.R
@@ -5,7 +5,7 @@
 
 bmpMerge <- merge(x = bmp0.5p, y = bmp1p, by = c('test','lab','substrate'), all = TRUE, suffixes = c('.bmp0.5p','.bmp1p'))
 
-bmpPlot <- bmpMerge[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p')]
+bmpPlot <- bmpMerge[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','rate.met.bmp1p','rate.met.bmp0.5p')]
 
 #add bmp20 and bmp30
 
@@ -13,23 +13,23 @@ bmpPlot <- bmpMerge[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p',
 #add bmp 20
 bmpMerge20 <- merge(x = bmpPlot, y = bmp20, by = c('test','lab','substrate'), all = TRUE, suffixes = c('.bmpPlot','.bmp20'))
 
-bmpPlot20 <- bmpMerge20[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d','BMP')]
+bmpPlot20 <- bmpMerge20[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','rate.met.bmp1p','rate.met.bmp0.5p','time.d','BMP')]
 
 #naming bmpPlot20
 
-names(bmpPlot20) <- c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d.BMP20','BMP20')
+names(bmpPlot20) <- c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','rate.met.bmp1p','rate.met.bmp0.5p','time.d.BMP20','BMP20')
 
 #Add bmp30
 
 bmpMerge30 <- merge(x = bmpPlot20, y = bmp30, by = c('test','lab','substrate'), all = TRUE)
 
 bmpPlot30 <- 
-  bmpMerge30[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d.BMP20','BMP20','time.d','BMP')]
+  bmpMerge30[,c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','rate.met.bmp1p','rate.met.bmp0.5p','time.d.BMP20','BMP20','time.d','BMP')]
 
 #naming bmpPlot30
 
 names(bmpPlot30) <- 
-  c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','time.d.BMP20','BMP20','time.d.BMP30','BMP30')
+  c('test','lab','substrate','time.d.bmp0.5p','time.d.bmp1p', 'BMP.bmp0.5p', 'BMP.bmp1p','rate.met.bmp1p','rate.met.bmp0.5p','time.d.BMP20','BMP20','time.d.BMP30','BMP30')
 
 
 #add gross vs net --> figure out if bmp0.5pg is possible to add first!
